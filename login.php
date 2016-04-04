9c989c81f25c87da96bf3ef89f9a2d24f81c9756
RichardSmithDev <thatonetechguy1999@gmail.com>
2016-04-03 22:44:21 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
 <style>
-/* Style The Dropdown Button */
 .dropbtn {
     background-color: #4CAF50;
     color: white;
@@ -9,13 +11,11 @@
     cursor: pointer;
 }
 
-/* The container <div> - needed to position the dropdown content */
 .dropdown {
     position: relative;
     display: inline-block;
 }
 
-/* Dropdown Content (Hidden by Default) */
 .dropdown-content {
     display: none;
     position: absolute;
@@ -24,7 +24,6 @@
     box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
 }
 
-/* Links inside the dropdown */
 .dropdown-content a {
     color: black;
     padding: 12px 16px;
@@ -32,19 +31,21 @@
     display: block;
 }
 
-/* Change color of dropdown links on hover */
 .dropdown-content a:hover {background-color: #f1f1f1}
 
-/* Show the dropdown menu on hover */
 .dropdown:hover .dropdown-content {
     display: block;
 }
 
-/* Change the background color of the dropdown button when the dropdown content is shown */
 .dropdown:hover .dropbtn {
     background-color: #3e8e41;
 }
 </style>
+</head>
+<body>
+
+<h2>Dropdown Menu</h2>
+<p>Move the mouse over the button to open the dropdown menu.</p>
 
 <div class="dropdown">
   <button class="dropbtn">Dropdown</button>
@@ -54,3 +55,8 @@
     <a href="#">Link 3</a>
   </div>
 </div>
+
+<p><strong>Note:</strong> We use href="#" for test links. In a real web site this would be URLs.</p>
+
+</body>
+</html>
